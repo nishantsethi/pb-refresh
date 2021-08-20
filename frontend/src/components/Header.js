697be9915e905828ec7c1855f47b47cdf0352b86
Nishant Sethi <sethi.nishant43@gmail.com>
2021-08-20 15:31:32 +0530

    backend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,34 @@
 import React from "react";
 import { Navbar, Nav, Container, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-        <Navbar.Brand href="/">BuyIt</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="mr-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="/cart"><i className="fas fa-shopping-cart" ></i>Cart</Nav.Link>
-            <Nav.Link href="/login"><i className="fas fa-user" ></i>Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          <LinkContainer to="/">
+            <Navbar.Brand>BuyIt</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="mr-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Login
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
