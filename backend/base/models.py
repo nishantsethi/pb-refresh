60caa0c9ed27c5a28ea6a099523d9040502747e4
Nishant Sethi <sethi.nishant43@gmail.com>
2021-08-24 21:01:29 +0530

    chup

--- backend/base/models.py
+++ backend/base/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    #image =
+    image = models.ImageField(null=True, blank= True)
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null = True, blank = True)
@@ -48,3 +48,28 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.createdAt)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null = True, blank = True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null = True, blank = True)
+    image = models.CharField(max_length=500, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.name)
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    address =models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postalCode = models.CharField(max_length=200, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null = True, blank = True)
+    id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.address)
